5745e2777f2b36b02f3f5c32d48f6b69e74bb352
Florian Plank <polarblau@gmail.com>
2015-05-03 12:49:21 +0200

    Sudo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,8 @@ set :user, 'flo'
 set :application, 'news_app'
 set :repo_url, 'git@github.com:polarblau/news.git'
 
+set :foreman_use_sudo, true
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
@@ -38,12 +40,12 @@ set :deploy_to, "/var/www/#{fetch(:application)}"
 namespace :deploy do
 
   after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+    # on roles(:web), in: :groups, limit: 3, wait: 10 do
+    #   # Here we can do anything such as:
+    #   # within release_path do
+    #   #   execute :rake, 'cache:clear'
+    #   # end
+    # end
   end
 
 end
