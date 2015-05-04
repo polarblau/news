b4ec638046f4b9a3b7f79760783c46f5668c2577
Florian Plank <polarblau@gmail.com>
2015-05-04 08:19:09 +0200

    Restart.

--- config/deploy.rb
+++ config/deploy.rb
@@ -101,11 +101,11 @@ namespace :foreman do
   desc "Restart the application services"
   task :restart do
     on roles(:app) do
-      execute "sudo stop #{fetch(:application)} || sudo start #{fetch(:application)}"
+      execute "sudo start #{fetch(:application)} || sudo restart #{fetch(:application)}"
     end
   end
 end
 
 after "deploy:publishing", "middleman:build"
-# after "deploy:publishing", "foreman:export"
+after "deploy:publishing", "foreman:export"
 after "deploy:publishing", "foreman:restart"
